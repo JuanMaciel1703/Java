a9b17e122a7e0f2e6989bd0c4223792cda436ba5
Juan Lucas Arruda Maciel <jmaciel172013@gmail.com>
2016-10-31 11:53:12 -0200

    Projeto Final MOD3
    Projeto desenvolvido com o intuito de aplicar as habilidades desenvolvidas durante o terceiro curso de Desenvolvimento de Aplicativos Mobile do IFSP Sertãozinho. Neste, há apenas aplicações básicas de integração com banco de dados (MySQL), onde problemas como o SQLi não foram tratados.

---
+++ ProjetoFinalMod3/src/Principal/Disciplina.java
@@ -0,0 +1,12 @@
+package Principal;
+
+//******************************************************
+//Instituto Federal de São Paulo - Campus Sertãozinho
+//Disciplina......: M3LPBD
+//Programação de Computadores e Dispositivos Móveis
+//Aluno...........: Juan Lucas Arruda Maciel
+//******************************************************
+
+public class Disciplina {
+    
+}
---
+++ ProjetoFinalMod3/src/Principal/Principal.java
@@ -0,0 +1,16 @@
+package Principal;
+
+//******************************************************
+//Instituto Federal de São Paulo - Campus Sertãozinho
+//Disciplina......: M3LPBD
+//Programação de Computadores e Dispositivos Móveis
+//Aluno...........: Juan Lucas Arruda Maciel
+//******************************************************
+
+public class Principal {
+
+    public static void main(String[] args) {
+        new Interfaces.Menu().setVisible(true);
+    }
+    
+}
